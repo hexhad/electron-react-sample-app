6062dd44d6e005d818fa04d276a9d0d7f82a57aa
Hashan Dharmapriya <hashan@mainframe.lk>
2024-11-28 11:19:32 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+```bash
+# yarn 1
+yarn create electron-app my-app
+
+# npm
+npx create-electron-app@latest my-app
+```
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-zinc-950">
+      <div className="font-bold text-white">Hello World</div>
+    </div>
+  );
+};
+
+export default App;
